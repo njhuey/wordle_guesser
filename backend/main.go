d1c7c29c2d2e0dc712708c651643cb88c9254123
Nathan Huey <nathanhuey44@gmail.com>
2025-01-01 20:56:48 -0800

    Add ability to eliminate possible words based on colored guess
    These colored guesses come directly from the Wordle site. If you would
    like to process multiple guesses at a time, simply run the functionality
    once per guess.

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"errors"
 	"fmt"
 	"os"
 	"unicode"
@@ -11,36 +12,61 @@ import (
 type PossibleColor int
 
 const (
-	GREY PossibleColor = iota
-	YELLOW
-	GREEN
+	Grey PossibleColor = iota
+	Yellow
+	Green
 )
 
 // A possible Wordle word with colored letter combinations.
 type ColoredWord struct {
 	letters string
-	colors  [5]PossibleColor
+	colors  []PossibleColor
 }
 
 // Ideally this should be a path stored in an env file but this is fine for now.
 const inputWordsFilename = "words.csv"
 
-// Check if a string is a valid Wordle word.
-func checkWord(word string) bool {
-	if len(word) != 5 {
-		return false
-	}
+// The complete list of words possible in Wordle are available at module init time.
+var allWords []string
+var wordSlices map[string][]rune
+var wordLetters map[string]map[rune]int
 
+// Check if a string is a valid Wordle word.
+func checkWord(word string) error {
 	for _, c := range word {
 		if !unicode.IsLetter(c) {
-			return false
+			return errors.New(
+				fmt.Sprintf("Word %s does not contain strictly English alphabetical letters, exiting.", word),
+			)
 		}
 	}
-	return true
+
+	constructedWord := []rune(word)
+	if len(constructedWord) != 5 {
+		return errors.New(fmt.Sprintf("Word %s is not 5 letters long, exiting.", word))
+	}
+
+	if len(word) != 5 {
+		return errors.New(fmt.Sprintf("Word %s is not valid because it contains characters that are not in the English alphabet, exiting.", word))
+	}
+	return nil
 }
 
-// Load all possible Wordle words from csv into memory.
-func loadAllWords() []string {
+func getLetterCount(word string) map[rune]int {
+	letterCount := make(map[rune]int)
+	for _, c := range word {
+		if _, ok := letterCount[c]; !ok {
+			letterCount[c] = 1
+		} else {
+			letterCount[c] += 1
+		}
+	}
+	return letterCount
+}
+
+// Load all possible Wordle words from csv into memory. Additionally, load in word slice
+// representations and letter counters.
+func loadAllWords() ([]string, map[string][]rune, map[string]map[rune]int) {
 	file, err := os.Open(inputWordsFilename)
 	if err != nil {
 		panic(err)
@@ -56,36 +82,101 @@ func loadAllWords() []string {
 	words := make([]string, len(content))
 	for i, row := range content {
 		word := row[1]
-		if !checkWord(word) {
+		err := checkWord(word)
+		if err != nil {
 			errorMessage := fmt.Sprintf(
-				"Loaded word '%s' from input csv file '%s' is invalid, exiting.",
+				"Loaded word '%s' from input csv file '%s' is invalid. Provided error: %s, exiting.",
 				word,
 				inputWordsFilename,
+				err,
 			)
 			panic(errorMessage)
 		}
 		words[i] = word
 	}
-	return words
-}
 
-func getLetterCount(word string) map[rune]int {
-	letterCount := make(map[rune]int)
-	for _, c := range word {
-		if _, ok := letterCount[c]; !ok {
-			letterCount[c] = 1
-		} else {
-			letterCount[c] += 1
-		}
+	wordSlices := make(map[string][]rune)
+	wordLetters := make(map[string]map[rune]int)
+
+	for _, word := range words {
+		wordSlices[word] = []rune(word)
+		wordLetters[word] = getLetterCount(word)
 	}
-	return letterCount
+	return words, wordSlices, wordLetters
 }
 
-func determineNextGuess(wordsGuessed []ColoredWord) string {
-	return "test"
+// Given a slice of remaining possible words and a previous guess colored word,
+// eliminate words that are no longer possible and return the slice.
+func eliminateImpossibleWords(remainingWords []string, guess ColoredWord) []string {
+	requiredLetters := make(map[rune]int)
+	cappedLetters := make(map[rune]bool)
+
+	for i := range 5 {
+		newWords := make([]string, 0, len(remainingWords))
+		currentLetter := wordSlices[guess.letters][i]
+
+		switch guess.colors[i] {
+		case Grey:
+			for _, word := range remainingWords {
+				if wordSlices[word][i] != currentLetter {
+					newWords = append(newWords, word)
+				}
+			}
+			cappedLetters[currentLetter] = true
+		case Yellow:
+			for _, word := range remainingWords {
+				if wordSlices[word][i] != currentLetter {
+					newWords = append(newWords, word)
+				}
+			}
+			if _, ok := requiredLetters[currentLetter]; !ok {
+				requiredLetters[currentLetter] = 1
+			} else {
+				requiredLetters[currentLetter] += 1
+			}
+		case Green:
+			for _, word := range remainingWords {
+				if wordSlices[word][i] == currentLetter {
+					newWords = append(newWords, word)
+				}
+			}
+			if _, ok := requiredLetters[currentLetter]; !ok {
+				requiredLetters[currentLetter] = 1
+			} else {
+				requiredLetters[currentLetter] += 1
+			}
+		}
+		remainingWords = newWords
+	}
+
+	for letter, requiredCount := range requiredLetters {
+		newWords := make([]string, 0, len(remainingWords))
+		for _, word := range remainingWords {
+			if wordLetters[word][letter] >= requiredCount {
+				newWords = append(newWords, word)
+			}
+		}
+		remainingWords = newWords
+	}
+
+	// If any grey letter has been encountered, the number of required letters must be
+	// the same as the number of actual letters in the word.
+	for letter := range cappedLetters {
+		newWords := make([]string, 0, len(remainingWords))
+		for _, word := range remainingWords {
+			if wordLetters[word][letter] == requiredLetters[letter] {
+				newWords = append(newWords, word)
+			}
+		}
+		remainingWords = newWords
+	}
+	return remainingWords
+}
+
+func init() {
+	allWords, wordSlices, wordLetters = loadAllWords()
 }
 
 func main() {
-	words := loadAllWords()
-	fmt.Println(words)
+	fmt.Println(allWords)
 }
--- backend/main_test.go
+++ backend/main_test.go
@@ -1,42 +1,98 @@
 package main
 
 import (
+	"maps"
+	"slices"
 	"testing"
 )
 
-func isMapEqual(m1 map[rune]int, m2 map[rune]int) bool {
-	if len(m1) != len(m2) {
-		return false
+func TestCheckHello(t *testing.T) {
+	word := "hello"
+	err := checkWord(word)
+	if err != nil {
+		t.Errorf("Word %s was incorrectly identified as invalid.", string(word))
 	}
+}
 
-	for k, v := range m1 {
-		if m2[k] != v {
-			return false
-		}
+func TestCheckHelë(t *testing.T) {
+	word := "helë"
+	err := checkWord(word)
+	if err == nil {
+		t.Errorf("Word %s was incorrectly identified as valid.", string(word))
 	}
-	return true
 }
 
 func TestLetterCountGreat(t *testing.T) {
 	word := "great"
 	letterCount := getLetterCount(word)
-	if !isMapEqual(letterCount, map[rune]int{'g': 1, 'r': 1, 'e': 1, 'a': 1, 't': 1}) {
-		t.Errorf("Letter count is incorrect for word: %s", word)
+	if !maps.Equal(letterCount, map[rune]int{'g': 1, 'r': 1, 'e': 1, 'a': 1, 't': 1}) {
+		t.Errorf("Letter count is incorrect for word: %s", string(word))
 	}
 }
 
 func TestLetterCountHello(t *testing.T) {
 	word := "hello"
 	letterCount := getLetterCount(word)
-	if !isMapEqual(letterCount, map[rune]int{'h': 1, 'e': 1, 'l': 2, 'o': 1}) {
-		t.Errorf("Letter count is incorrect for word: %s", word)
+	if !maps.Equal(letterCount, map[rune]int{'h': 1, 'e': 1, 'l': 2, 'o': 1}) {
+		t.Errorf("Letter count is incorrect for word: %s", string(word))
 	}
 }
 
 func TestLetterCountTests(t *testing.T) {
 	word := "tests"
 	letterCount := getLetterCount(word)
-	if !isMapEqual(letterCount, map[rune]int{'t': 2, 'e': 1, 's': 2}) {
-		t.Errorf("Letter count is incorrect for word: %s", word)
+	if !maps.Equal(letterCount, map[rune]int{'t': 2, 'e': 1, 's': 2}) {
+		t.Errorf("Letter count is incorrect for word: %s", string(word))
+	}
+}
+
+func TestEliminateWordsExact(t *testing.T) {
+	remainingWords := []string{"hello", "great", "sword"}
+	guess := ColoredWord{
+		letters: "great",
+		colors:  []PossibleColor{Green, Green, Green, Green, Green},
+	}
+
+	result := eliminateImpossibleWords(remainingWords, guess)
+	if len(result) != 1 {
+		t.Errorf("Incorrect number of remaining words returned: %d. Expected: 1", len(result))
+	}
+
+	if result[0] != "great" {
+		t.Errorf("Incorrect final word remaining: %s", result[0])
+	}
+}
+
+func TestEliminateWordsPartialGreen(t *testing.T) {
+	remainingWords := []string{"cares", "sport", "sworn", "sword"}
+	guess := ColoredWord{
+		letters: "sport",
+		colors:  []PossibleColor{Green, Grey, Green, Green, Grey},
+	}
+
+	result := eliminateImpossibleWords(remainingWords, guess)
+	if len(result) != 2 {
+		t.Errorf("Incorrect number of remaining words returned: %d. Expected 2", len(result))
+	}
+
+	if !slices.Equal(result, []string{"sworn", "sword"}) {
+		t.Errorf("Incorrect words remaining: %s", result)
+	}
+}
+
+func TestEliminateWordsPartialMixed(t *testing.T) {
+	remainingWords := []string{"cares", "rains", "fairs", "hairs"}
+	guess := ColoredWord{
+		letters: "rains",
+		colors:  []PossibleColor{Yellow, Green, Green, Grey, Green},
+	}
+
+	result := eliminateImpossibleWords(remainingWords, guess)
+	if len(result) != 2 {
+		t.Errorf("Incorrect number of remaining words returned: %d. Expected 2", len(result))
+	}
+
+	if !slices.Equal(result, []string{"fairs", "hairs"}) {
+		t.Errorf("Incorrect words remaining: %s", result)
 	}
 }
